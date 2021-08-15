4aa95ef06e898e927f6cb7d2492d6998edc3c80e
Brian Rogers <6869577+brogers5@users.noreply.github.com>
2021-08-14 20:30:34 -0400

    Update to 11.0.1.49938

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 # See the comments in https://github.com/brogers5/chocolatey-package-foxitreader/blob/main/update.ps1
 # on the &language=German part.
-$url32       = 'https://www.foxit.com/downloads/latest.html?product=Foxit-Reader&platform=Windows&package_type=exe&language=German&version=11.0.0.49893'
-$checksum32  = '6d33ca56b5c6e7f412ff7e7af6a78036df4e7affd1754975ca9a0a89df9d570c'
+$url32       = 'https://www.foxit.com/downloads/latest.html?product=Foxit-Reader&platform=Windows&package_type=exe&language=German&version=11.0.1.49938'
+$checksum32  = 'd6281d2029e46a2203dbf94d302f26213e264be2dee108e7cfd209bb13b15666'
 
 # FoxitReader changes its offered installer from time to time between two versions
 #  a) the installer comes with a wrapper setup
