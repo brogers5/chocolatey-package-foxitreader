a5be1fcc64ab68df78a1bab1ccee0c0e5ab7940d
brogers5 <6869577+brogers5@users.noreply.github.com>
2021-11-02 23:29:32 -0400

    Extract wrapper-installer functions to simplify install script.

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -15,88 +15,12 @@ $isWrappedInstaller = $false
 
 if ($isWrappedInstaller)
 {
-	<#
-	.SYNOPSIS
-	Download the current version and unwrap the inner native installer.
-	
-	Return a tuple ($pathToUnwrappedNativeInstaller, $tmpDirectory) where
-	$tmpDirectory is the directory where the native installer is contained
-	and which should ideally be deleted after installation.
-	#>
-	function Get-CurrentVersion {
-		Write-Output 'Downloading current version...' | Out-Null
-	
-		# FoxitReader has recently changed to a "wrapped" setup program:
-		# The above $url32 will download a wrapper setup EXE, which is more or less
-		# a GUI interface to the wrapped inner setup EXE.
-		#
-		# The wrapper setup neither accepts CLI arguments for silencing nor for
-		# configuration as far as we know.
-		# Therefore, after downloading the wrapper setup, we extract the wrapped
-		# setup EXE and run it manually in Install-CurrentVersion.
-	
-		$toolsDir = $(Split-Path -Parent $MyInvocation.ScriptName)
-		$wrapperSetupDownloadPath = $(Join-Path $toolsDir "wrapper-setup.exe")
-		$wrapperSetupUnzippedPath = $(Join-Path $toolsDir "wrapper-setup-extracted")
-	
-		$wrapperDownloadArgs = @{
-			PackageName    = 'foxitreader'
-			FileFullPath   = $wrapperSetupDownloadPath
-			Url            = $url32
-			Checksum       = $checksum32
-			ChecksumType   = 'sha256'
-			Options = @{
-				Headers = @{
-					# In addition on the '/de/' part in the URL (which actually forces
-					# an English language setup), also specify the desired language as
-					# an HTTP header even though this had no effect in the past.
-					'Accept-Language' = 'de-DE'
-				}
-			}
-		}
-	
-		$wrapperUnzipArgs = @{
-			PackageName    = 'foxitreader'
-			FileFullPath   = $wrapperSetupDownloadPath
-			Destination    = $wrapperSetupUnzippedPath
-		}
-	
-		Get-ChocolateyWebFile @wrapperDownloadArgs | Out-Null
-		Get-ChocolateyUnzip @wrapperUnzipArgs | Out-Null
-	
-		# Do not use gci -File parameter to retain PS 2 compatibility
-		$wrappedSetupPath =
-			Get-ChildItem `
-				-Path $(Join-Path $wrapperSetupUnzippedPath '$PLUGINSDIR') `
-				'FoxitReader*Setup*.exe' `
-			| Where-Object {! $_.PSIsContainer} | Select-Object -First 1 -ExpandProperty FullName
-	
-		# Continue and don't fail in case AV software locks files
-		Remove-Item $wrapperSetupDownloadPath -Recurse -Force -ErrorAction Continue
-	
-		return $wrappedSetupPath, $wrapperSetupUnzippedPath
-	}
-	
-	function Install-CurrentVersion([string] $installerPath) {
-		Write-Output 'Installing current version...'
-	
-		$installationArgs = @{
-			PackageName    = 'foxitreader'
-			FileType       = 'EXE'
-			File           = $installerPath
-			silentArgs     = '/verysilent'
-			validExitCodes = @(0)
-		}
-		Write-Host $installerPath
-		Install-ChocolateyInstallPackage @installationArgs
-	}
-	
 	Write-Output 'Downloading current version and then uninstalling previous version (if any) before performing installation of the current version.'
 	
 	$tmpDirectory = $null
 	
 	try {
-		$installerPath, $tmpDirectory = Get-CurrentVersion
+		$installerPath, $tmpDirectory = Get-CurrentVersion $url32 $checksum32
 		Write-Output 'Uninstalling previous version...'
 		Uninstall-FoxitPDFReader
 		Install-CurrentVersion $installerPath
