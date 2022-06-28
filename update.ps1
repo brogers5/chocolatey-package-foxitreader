557a4ede39fbf1942e57cbd03ed82c4405a8b366
brogers5 <6869577+brogers5@users.noreply.github.com>
2022-06-28 19:33:30 -0400

    Remove unused variable LatestVersion

--- update.ps1
+++ update.ps1
@@ -50,39 +50,10 @@ function global:au_GetLatest {
 
     $url32 = "https://cdn06.foxitsoftware.com/product/reader/desktop/win/$($version.Major).$($version.Minor).$($version.Build)/FoxitPDFReader$($version.Major)$($version.Minor)$($version.Build)_L10N_Setup_Prom.exe"
 
-    $actualVersion = $version
-
-    # 9.0.1.1050: Fix for issue #5: Checksum fails due to IP-specific language redirect
-    # 9.0.1.1051: Fix for a modified binary on Foxit's servers:
-    #             https://chocolatey.org/packages/FoxitReader#comment-3834103556
-    # 9.0.1.1052: Fix for a modified binary on Foxit's servers:
-    #             https://chocolatey.org/packages/FoxitReader#comment-3839195230
-    # 9.1.0.5097: Fix for issue #7: PowerShell 2 does not support -File parameter of Get-ChildItem
-    # 9.3.0.10827: Foxit Software Inc. published a no-wrapper setup (version 9.3.0.10826), then this package's
-    #              version 9.3.0.10826 had been published, then Foxit changed its published setup to include
-    #              a wrapper (again with version 9.3.0.10826).
-    # 9.3.0.10828: chocolateyInstall.ps1 from 9.3.0.10827 contained an empty checksum string
-    # 9.3.0.10829: Fix for a modified binary on Foxit's servers:
-    #              https://chocolatey.org/packages/FoxitReader/9.3.0.10829#comment-4186580031
-    # 9.4.1.16829: Fix for a modified binary on Foxit's servers + package was rejected due to being left
-    #              untouched for more than 14 days after automatic test failing:
-    #              https://chocolatey.org/packages/FoxitReader/9.4.1.16828
-    if ($version -eq "9.1.0.5096") {
-        $version = "9.1.0.5097"
+    return @{
+        Url32 = $url32
+        Version = $version
     }
-    elseif ($version -eq "9.3.0.10826") {
-        $version = "9.3.0.10829"
-    }
-    elseif ($version -eq "9.4.1.16828") {
-        $version = "9.4.1.16829"
-    }
-    elseif ($version -in @("9.0.1.1050", "9.0.1.1051", "9.0.1.1052", "9.1.0.5097", "9.3.0.10827", "9.3.0.10828", "9.3.0.10829", "9.4.1.16829")) {
-        Write-Error -Message @"
-FoxitReader's current version collides with a version used as package fix notation.
-"@
-    }
-
-    return @{ Url32 = $url32; Version = $version; ActualVersion = $actualVersion }
 }
 
 Update-Package -ChecksumFor 32 -NoReadme
