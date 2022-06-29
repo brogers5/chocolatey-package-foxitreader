63762459ba5475687764006f84b11aaa24712631
brogers5 <6869577+brogers5@users.noreply.github.com>
2022-06-28 21:10:56 -0400

    Source package name from environment variable

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $url32       = 'https://cdn06.foxitsoftware.com/product/reader/desktop/win/11.2.
 $checksum32  = 'fc969c7fac7f0b97e8b92b74a4af64c20ddb25f9526a23eb7ebd120ea43a9c22'
 
 $installationArgs = @{
-    PackageName    = 'foxitreader'
+    PackageName    = $env:ChocolateyPackageName
     FileType       = 'EXE'
     Url            = $url32
     Checksum       = $checksum32
