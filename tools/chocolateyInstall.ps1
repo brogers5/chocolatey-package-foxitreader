28b4256e81b6a0206a2cf5320a501ec30f6f5560
brogers5 <6869577+brogers5@users.noreply.github.com>
2022-06-28 21:11:47 -0400

    Make splat argument casing consistent (camelCase)

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -4,11 +4,11 @@ $url32       = 'https://cdn06.foxitsoftware.com/product/reader/desktop/win/11.2.
 $checksum32  = 'fc969c7fac7f0b97e8b92b74a4af64c20ddb25f9526a23eb7ebd120ea43a9c22'
 
 $installationArgs = @{
-    PackageName    = $env:ChocolateyPackageName
-    FileType       = 'EXE'
-    Url            = $url32
-    Checksum       = $checksum32
-    ChecksumType   = 'sha256'
+    packageName    = $env:ChocolateyPackageName
+    fileType       = 'EXE'
+    url            = $url32
+    checksum       = $checksum32
+    checksumType   = 'sha256'
     silentArgs     = '/verysilent'
     validExitCodes = @(0)
 }
