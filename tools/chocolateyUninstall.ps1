bda612958a503b8a1281c7a0b79a9daccb6edf46
brogers5 <6869577+brogers5@users.noreply.github.com>
2022-09-12 20:14:56 -0400

    Add warning for software being uninstalled outside of Chocolatey

--- tools/chocolateyUninstall.ps1
+++ tools/chocolateyUninstall.ps1
@@ -14,6 +14,9 @@ if ($key.Count -eq 1) {
     $packageArgs['file'] = $($key[0].UninstallString)
     Uninstall-ChocolateyPackage @packageArgs
 }
+elseif ($key.Count -eq 0) {
+    Write-Warning "$packageName has already been uninstalled by other means."
+}
 elseif ($key.Count -gt 1) {
     Write-Warning "$($key.Count) matches found!"
     Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
