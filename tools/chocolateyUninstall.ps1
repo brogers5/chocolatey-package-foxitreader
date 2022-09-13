608a5cc68ea511ca23b7f2d76e2e5c5259d2e98e
brogers5 <6869577+brogers5@users.noreply.github.com>
2022-09-12 20:25:14 -0400

    Rename $key to better represent intended return type

--- tools/chocolateyUninstall.ps1
+++ tools/chocolateyUninstall.ps1
@@ -8,20 +8,20 @@ $packageArgs = @{
     validExitCodes= @(0)
 }
 
-[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+[array] $keys = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
 
-if ($key.Count -eq 1) {
-    $packageArgs['file'] = $($key[0].UninstallString)
+if ($keys.Count -eq 1) {
+    $packageArgs['file'] = $($keys[0].UninstallString)
     Uninstall-ChocolateyPackage @packageArgs
 }
-elseif ($key.Count -eq 0) {
+elseif ($keys.Count -eq 0) {
     Write-Warning "$packageName has already been uninstalled by other means."
 }
-elseif ($key.Count -gt 1) {
-    Write-Warning "$($key.Count) matches found!"
+elseif ($keys.Count -gt 1) {
+    Write-Warning "$($keys.Count) matches found!"
     Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
     Write-Warning "Please alert package maintainer the following keys were matched:"
-    $key | ForEach-Object {Write-Warning "- $($_.DisplayName)"}
+    $keys | ForEach-Object {Write-Warning "- $($_.DisplayName)"}
 }
 
 Uninstall-BinFile -Name 'FoxitPDFReader'
