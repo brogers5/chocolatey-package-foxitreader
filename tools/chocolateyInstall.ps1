efee472e1aacc5b8685a1173527554d56ab4c3db
brogers5 <6869577+brogers5@users.noreply.github.com>
2023-04-20 00:11:18 -0400

    Add logging to default arguments

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -6,7 +6,7 @@ $installationArgs = @{
     url            = 'https://cdn01.foxitsoftware.com/product/reader/desktop/win/12.1.2/FoxitPDFReader1212_L10N_Setup_Prom.exe'
     checksum       = '2ad426023460ad745b8d6317d956219b74353bcc817c4b5553371978c3f5cc98'
     checksumType   = 'sha256'
-    silentArgs     = '/verysilent'
+    silentArgs     = "/verysilent /log=`"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).Install.log`""
     validExitCodes = @(0)
 }
 
