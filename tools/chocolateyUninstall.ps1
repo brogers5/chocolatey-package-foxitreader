b961ab85ac3ec8b16bc70c0c46374675ae6803b7
brogers5 <6869577+brogers5@users.noreply.github.com>
2025-09-25 13:56:20 -0400

    Overhaul uninstall script with current implementation

--- tools/chocolateyUninstall.ps1
+++ tools/chocolateyUninstall.ps1
@@ -3,24 +3,25 @@
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = 'Foxit PDF Reader'
-    fileType       = 'EXE'
-    silentArgs     = "/uninstall /quiet /norestart /log `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).Uninstall.log`""
-    validExitCodes = @(0)
+    fileType       = 'MSI'
+    silentArgs     = '/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).Uninstall.log`"'
+    validExitCodes = @(0, 3010, 1605, 1614, 1641)
 }
 
 [array] $keys = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
 
-if ($keys.Count -eq 0) {
+if ($keys.Count -eq 1) {
+    $keys | ForEach-Object {
+        $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
+        $packageArgs['file'] = ''
+
+        Uninstall-ChocolateyPackage @packageArgs
+    }
+}
+elseif ($keys.Count -eq 0) {
     Write-Warning "$packageName has already been uninstalled by other means."
 }
-elseif ($keys.Count -le 2) {
-    #Foxit PDF Reader adds a couple keys. We need to use the one for the bundle bootstrapper application.
-    $key = $keys | Where-Object { ($_.PSObject.Properties['BundleCachePath']) }
-
-    $packageArgs['file'] = $($key.UninstallString.TrimEnd('  /uninstall'))
-    Uninstall-ChocolateyPackage @packageArgs
-}
-elseif ($keys.Count -gt 2) {
+elseif ($keys.Count -gt 1) {
     Write-Warning "$($keys.Count) matches found!"
     Write-Warning 'To prevent accidental data loss, no programs will be uninstalled.'
     Write-Warning 'Please alert package maintainer the following keys were matched:'
